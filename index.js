1f8be42245e9fd079b5dad0a5b137122cbccf5c7
janbelarus <janpiatlicki@gmail.com>
2022-06-09 11:25:26 +0300

    feat: add js

--- index.js
+++ index.js
@@ -22,28 +22,6 @@ navItem.forEach((item) => {
    let icon = document.getElementById ("icon");
 let img = document.getElementById ("logo");
 
-icon.onclick = function(){
-    document.body.classList.toggle ("light-theme");
-    if (document.body.classList.contains("light-theme")){
-        icon.src = "./assets/svg/Header/moon.svg";
-        img.src = "./assets/svg/Header/black_logo.svg";
-        insta.src ="./assets/svg/footer/VectorInst.svg";
-        face.src="./assets/svg/footer/VectorFace.svg";
-        twit.src="./assets/svg/footer/VectorTwitter.svg";
-        pint.src="./assets/svg/footer/VectorPint.svg";
-        
-    }
-    else{
-        icon.src = "./assets/svg/Header/carbon_sun.svg";
-        img.src = "./assets/svg/Header/logo.svg";
-        insta.src = "./assets/svg/footer/Vector.svg";
-        face.src="./assets/svg/footer/Vector-1.svg";
-        twit.src="./assets/svg/footer/Vector-2.svg";
-        pint.src="./assets/svg/footer/Vector-3.svg"
-
-    }
-  }
-
 const contact = document.querySelector (".contact");
 const header = document.querySelector (".header");
 icon.addEventListener("click", () => {
@@ -51,18 +29,18 @@ icon.addEventListener("click", () => {
     contact.classList.toggle("open");
 });
 
-const portfolioButtons = document.querySelector('.portfolio-buttons');
-const portfolioButton = document.querySelectorAll('.portfolio-button');
-const portfolioImages = document.querySelectorAll('.portfolio-image');
-const seasons = ['winter', 'spring', 'summer', 'autumn'];
+const photoButtons = document.querySelector('.photo-buttons');
+const photoButton = document.querySelectorAll('.photo-button');
+const photoImages = document.querySelectorAll('.photo-image');
+const seasons = ['first', 'second', 'third', 'fourth'];
 
-portfolioButtons.addEventListener('click', changeImage);
+photoButtons.addEventListener('click', changeImage);
 
 function changeImage(event) {
-    if (event.target.classList.contains('portfolio-button')) {
-      portfolioButton.forEach((button) => button.classList.remove('active'));
+    if (event.target.classList.contains('photo-button')) {
+      photoButton.forEach((button) => button.classList.remove('active'));
       event.target.classList.add('active');
-      portfolioImages.forEach((img, index) => img.src = `./assets/img/${event.target.dataset.season}/${index + 1}.jpg` );
+      photoImages.forEach((img, index) => img.src = `./assets/img/${event.target.dataset.season}/${index + 1}.jpg` );
     }
   }
 
