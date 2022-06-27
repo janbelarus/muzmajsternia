4956849aa235b9a7e5ebcd1891891700d22b58c4
janbelarus <janpiatlicki@gmail.com>
2022-06-28 00:00:28 +0300

    fix: fix photo

--- index.js
+++ index.js
@@ -1,46 +1,15 @@
-import { initTranslation } from './translate.js';
-initTranslation();     
-const menuButton = document.querySelector(".menu");
-const navList = document.querySelector(".nav-list");
-const navItem = document.querySelectorAll(".nav-item");
+const portfolioButtons = document.querySelector('.portfolio-buttons');
+const portfolioButton = document.querySelectorAll('.portfolio-button');
+const portfolioImages = document.querySelectorAll('.portfolio-image');
+const seasons = ['winter', 'spring', 'summer', 'autumn'];
 
-menuButton.addEventListener("click", () => {
-    menuButton.classList.toggle("open");
-    navList.classList.toggle("open");
-});
-
-navItem.forEach((item) => {
-    item.addEventListener("click", () => {
-        if (menuButton.classList.contains("open")){
-          navList.classList.toggle("open");
-          menuButton.classList.toggle("open");
-
-        }
-    })
-   });
-
-   let icon = document.getElementById ("icon");
-let img = document.getElementById ("logo");
-
-const contact = document.querySelector (".contact");
-const header = document.querySelector (".header");
-icon.addEventListener("click", () => {
-    header.classList.toggle("open");
-    contact.classList.toggle("open");
-});
-
-const photoButtons = document.querySelector('.photo-buttons');
-const photoButton = document.querySelectorAll('.photo-button');
-const photoImages = document.querySelectorAll('.photo-image');
-const seasons = ['first', 'second', 'third', 'fourth'];
-
-photoButtons.addEventListener('click', changeImage);
+portfolioButtons.addEventListener('click', changeImage);
 
 function changeImage(event) {
-    if (event.target.classList.contains('photo-button')) {
-      photoButton.forEach((button) => button.classList.remove('active'));
+    if (event.target.classList.contains('portfolio-button')) {
+      portfolioButton.forEach((button) => button.classList.remove('active'));
       event.target.classList.add('active');
-      photoImages.forEach((img, index) => img.src = `./assets/img/${event.target.dataset.season}/${index + 1}.jpg` );
+      portfolioImages.forEach((img, index) => img.src = `./assets/img/${event.target.dataset.season}/${index + 1}.jpg` );
     }
   }
 
@@ -52,16 +21,3 @@ function preloadImages(season) {
   }
 
   seasons.forEach(season => preloadImages(season));
-
-  function setLocalStorage() {
-    localStorage.setItem('lang', lang);
-  }
-  window.addEventListener('beforeunload', setLocalStorage)
-
-  function getLocalStorage() {
-    if(localStorage.getItem('lang')) {
-      const lang = localStorage.getItem('lang');
-      getTranslate(lang);
-    }
-  }
-  window.addEventListener('load', getLocalStorage)
